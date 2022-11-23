97852b83c95eb4223897125eac72ac4eddacb8dd
cassepipe <cassepipe@ymail.com>
2022-11-23 15:57:30 +0100

    Revert "Added isAExecutableRegFile"
    This reverts commit 6be6b770cce0054f7428cb1401e562a69435bb3d.

--- includes/http/RequestHandler.hpp
+++ includes/http/RequestHandler.hpp
@@ -204,7 +204,6 @@ namespace HTTP
 			void						_parseGeneralHeaderFields();
 
 			bool	_isAReadableRegFile(const char* path);
-			bool	_isAExecutableRegFile(const char* path);
 
 			void	_methodInitGet();
 			void	_methodInitPost();
