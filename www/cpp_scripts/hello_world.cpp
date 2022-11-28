b46caa4061a321c97b786e589ca6881ed236d190
cassepipe <cassepipe@ymail.com>
2022-11-28 17:13:16 +0100

    Why header is not detected ?

--- www/cpp_scripts/hello_world.cpp
+++ www/cpp_scripts/hello_world.cpp
@@ -2,6 +2,8 @@
 
 int main(void)
 {
+	std::cout << "Content-type: text/html";
+	std::cout << "\r\n\r\n";
 	std::cout << "<html><head>Generated Hello World</head><body>";
 	std::cout << "Hello World";
 	std::cout << "</body><html>";
--- www/cpp_scripts/printenv.cpp
+++ www/cpp_scripts/printenv.cpp
@@ -2,6 +2,8 @@
 
 int main(int ac, char **av, char **envp)
 {
+	std::cout << "Content-type: text/plain";
+	std::cout << "\r\n\r\n";
 	if (envp)
 	{
 		for (; *envp != NULL ; ++envp)
