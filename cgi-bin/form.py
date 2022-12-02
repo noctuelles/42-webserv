7ab0cc07f93a58b4d1782ec6cdec434089dd6872
noctuelles <plouvel@student.42.fr>
2022-12-02 16:01:38 +0100

    new config files

---
+++ cgi-bin/form.py
@@ -0,0 +1,16 @@
+import cgi, cgitb 
+
+form = cgi.FieldStorage() 
+
+first_name = form.getvalue('first_name')
+last_name  = form.getvalue('last_name')
+
+print ("Content-type:text/html\r\n\r\n")
+print ("<html>")
+print ("<head>")
+print ("<title>Hello - Basic CGI Program</title>")
+print ("</head>")
+print ("<body>")
+print ("<h2>Hello %s %s</h2>" % (first_name, last_name))
+print ("</body>")
+print ("</html>")
